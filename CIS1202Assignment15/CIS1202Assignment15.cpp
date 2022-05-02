0de0e2c056ace53019870d525ef67e6ec540edd9
Kacey Hall <kaceyhall61@gmail.com>
2022-05-02 09:41:06 -0400

    invalidCharacterException and invalidRangeException added. Main function loops to complete the process.

--- CIS1202Assignment15/CIS1202Assignment15.cpp
+++ CIS1202Assignment15/CIS1202Assignment15.cpp
@@ -8,12 +8,47 @@ CIS 1202
 
 using namespace std;
 
+void Validate(char c)
+{
+    string invalidCharacterExcpeption = "Error: must enter a letter.";
+
+    if (c >= 'A' && c <= 'Z' || c >= 97 && c <= 122)
+    {
+        return;
+    }
+    else
+    {
+        throw invalidCharacterExcpeption;
+    }   
+}
+
+void ValidateOffset(char start, int offset)
+{
+    char newC = start + offset;
+
+    string invalidRangeException = "Offset was out of bounds.";
+
+    if (start >= 'a' && start <= 'z' && newC < 'a' || newC > 'z')
+    {
+        throw invalidRangeException;
+    }
+}
+
 char character(char start, int offset)
 {
     char newC;
+    string invalidRangeException = "Offset was out of bounds.";
 
     newC = start + offset;
 
+    if (start >= 'a' && start <= 'z')
+    {
+        ValidateOffset(start, offset);
+    }
+    else if (start >= 'A' && start <= 'Z' && newC < 'A' || newC > 'Z')
+    {
+        throw invalidRangeException;
+    }
 
     return newC;
 }
@@ -22,14 +57,38 @@ int main()
 {
     char x;
     int y;
+    bool again = true;
 
-    cout << "Pass a character: " << endl;
-    cin >> x;
-    cin.ignore();
+    while (again)
+    {
+        try
+        {
+            cout << "Pass a character: " << endl;
+            cin >> x;
+            cin.ignore();
 
-    cout << "Pass the offset: " << endl;
-    cin >> y;
-    cin.ignore();
+            Validate(x);
 
-    cout << character(x, y) << endl;
+            cout << "Pass the offset: " << endl;
+            cin >> y;
+            cin.ignore();
+
+            try
+            {
+                cout << character(x, y) << endl;
+            }
+            catch (string invalidRangeException)
+            {
+                cout << invalidRangeException << endl;
+            }
+        }
+        catch (string notALetter)
+        {
+            cout << notALetter << endl;
+        }         
+        
+        cout << "Enter more? (1 for more, 0 if finished.)" << endl;
+        cin >> again;
+        cin.ignore();
+    }
 }
